b3b5f54d304829019c1cf742c36cfaf2028394e9
Dmitriy Yarmaliuk <dmitriy.yarmaliuk@gmail.com>
2018-05-21 23:04:34 -0400

    Adding validation for POST

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
 const { fork } = require('child_process');
+const Joi = require('joi')
+
+const app = express();
 
 // Enabling parson of JSON object in the body of request (Middleware)
 app.use(express.json())
@@ -8,22 +10,14 @@ app.use(express.json())
 // Array containing all alerts received via the app or the echo
 let alerts = []
 
+// Defining how the alert should be structured
+const alertSchema = {
+    deviceType: Joi.string().required(),
+    location: Joi.string().required(),
+}
+
 app.get('/', (req, res) => {
     res.send('Welcome to MLL');
-});
-
-app.get('/pullData', (req, res) => {
-    const forked = fork('./processes/child.js');
-
-    forked.on('message', (msg) => {
-      console.log('Message from child', msg);
-      if (msg.status === 'done'){
-          console.log('Killing child process because it is done')
-          forked.kill()
-      }
-    });
-    
-    forked.send('Starting child process');
 });
 
 // Example post request:http://localhost:3000/api/alert
@@ -33,27 +27,33 @@ app.get('/pullData', (req, res) => {
 // }
 // Postman set to raw, JSON
 app.post('/api/alert', (req, res) => {
-    let alertFromClient = {
-        id: alerts.length + 1,
-        deviceType: req.body.deviceType,
-        location: req.body.location
-    }
-
-    alerts.push(alertFromClient)
-    res.send(alertFromClient)
-
-    const forked = fork('./processes/child.js');
-
-    forked.on('message', (msg) => {
-      console.log('Message from child', msg);
-      if (msg.status === 'done'){
-          console.log('Killing child process because it is done')
-          forked.kill()
-      }
-    });
+    let result = Joi.validate(req.body, alertSchema)
+    if (result.error){
+        res.status(400).send(result.error)
+        return
+    } else {
+        let alertFromClient = {
+            id: alerts.length + 1,
+            deviceType: req.body.deviceType,
+            location: req.body.location
+        }
     
-    forked.send('Starting child process');
-
+        alerts.push(alertFromClient)
+        res.send(alertFromClient)
+    
+        const forked = fork('./processes/child.js');
+    
+        forked.on('message', (msg) => {
+          console.log('Message from child', msg);
+          if (msg.status === 'done'){
+              console.log('Killing child process because it is done')
+              forked.kill()
+              return
+          }
+        });
+        
+        forked.send('Starting child process');
+    }
 });
 
 const port = process.env.PORT || 3000
